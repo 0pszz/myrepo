d45f9e1dc1f00d7a23fa53a4d2d9444843180f31
melo <110198757+0pszz@users.noreply.github.com>
2024-11-22 12:45:31 +0300

    Update settings.py

--- dumbbells/settings.py
+++ dumbbells/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-esq$()9-#=mp5&izdm@pv$3u)+is7@+=#v9xkjpqg-vnii&=9*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
